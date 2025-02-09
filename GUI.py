d63d6b16182f9bad6c0fc835fec18c11e48b5450
Luisalva244 <luis4789@hotmail.es>
2025-02-08 23:45:05 -0600

    fix: serial read

--- GUI.py
+++ GUI.py
@@ -1,15 +1,37 @@
 import serial
 import time
+import re
 
 
-esp32 = serial.Serial(port= '/dev/ttyUSB1', baudrate=115200, timeout=1)
 
-try:
+esp32 = serial.Serial(port= '/dev/ttyUSB0', baudrate=115200, timeout=1)
+
+def GetSerial():
     while True:
         if esp32.in_waiting > 0:  # Check if there's data available to read
-            data = esp32.readline().decode('utf-8').strip()  # Read and decode
-            print(f"Received: {data}")  # Print received data
+            uselessData = esp32.readline().decode('utf-8').strip()  # Read and decode
+            nodeData = esp32.readline().decode('utf-8').strip() 
+            humidityData = esp32.readline().decode('utf-8').strip() 
+            print(uselessData)
+            return nodeData + humidityData
 
-except KeyboardInterrupt:
-    print("\nClosing connection...")
-    esp32.close()
+
+        
+
+def nodeNumber(sData):
+    pattern = r'^Node:\s*(\d+)$'
+    
+    match = re.match(pattern, sData)
+    if match:
+        node_value = match.group(1)
+        return node_value
+    
+def main_loop():
+    while True:
+        Node = nodeNumber(GetSerial())
+        print(Node)
+
+
+if __name__ == "__main__":
+    main_loop()
+    
