e11a42939b5d63d16f11a0eed4e7655d69cee258
Luisalva244 <luis4789@hotmail.es>
2025-02-09 21:23:54 -0600

    chore: no changes

--- Main.py
+++ Main.py
@@ -13,19 +13,14 @@ def main():
 
     manager = DataManager(reader=serial_reader, parsers=parsers)
     
-    try:
-        while True:
-            data = manager.process_next_line()
-            if data:
-                tipo = data['type']
-                valor = data['value']
-                print(f"{tipo.capitalize()}: {valor}")
+    while True:
+        data = manager.process_next_line()
+        if data:
+            tipo = data['type']
+            valor = data['value']
+            print(f"{tipo.capitalize()}: {valor}")
             
             time.sleep(0.02)
-    except KeyboardInterrupt:
-        print("[INFO] Interrupción por teclado. Saliendo...")
-    finally:
-        serial_reader.close()
 
 if __name__ == "__main__":
     main()
